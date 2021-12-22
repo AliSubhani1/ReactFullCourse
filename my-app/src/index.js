920e7e07f6d3ee2f0a10c290a8733e8747cd16b5
Ali Subhani <ali.subhani@tintash.com>
2021-12-22 10:04:06 +0500

    created a list of books and passing the list object in props for rendering all the available books

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,38 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-
+const books = [
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51KvAoff+ZL._SX407_BO1,204,203,200_.jpg",
+    title: "Atlas of the Heart",
+    author: "Brené Brown",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71eA+3k5PzL._AC_UL210_SR195,210_.jpg",
+    title: "Atlas of the Heart 2",
+    author: "Brené Brown",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71eA+3k5PzL._AC_UL210_SR195,210_.jpg",
+    title: "Atlas of the Heart 3",
+    author: "Brené Brown",
+  },
+];
+const author = "Brené Brown";
+const title = "Atlas of the Heart";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/51KvAoff+ZL._SX407_BO1,204,203,200_.jpg";
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book book={book} />;
+      })}
     </section>
   );
 };
-const Book = () => {
+
+const Book = (props) => {
+  const { img, title, author, children } = props.book;
+  // console.log(props);
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img}></img>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{children}</p>
     </article>
   );
 };
 
-const Image = () => {
-  return (
-    <img src="https://images-na.ssl-images-amazon.com/images/I/51KvAoff+ZL._SX407_BO1,204,203,200_.jpg"></img>
-  );
-};
-const Title = () => {
-  return <h1>Atlas of the Heart</h1>;
-};
-const Author = () => {
-  return <h4>Brené Brown</h4>;
-};
 ReactDOM.render(<BookList />, document.getElementById("root"));
